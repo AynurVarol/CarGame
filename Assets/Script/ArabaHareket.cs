06923aaba6452200fbe96fb3beebdce402af735f
UfukSenkal <67637340+UfukSenkal@users.noreply.github.com>
2023-05-08 20:19:03 +0300

    single responsibility example

--- Assets/Script/ArabaHareket.cs
+++ Assets/Script/ArabaHareket.cs
@@ -9,14 +9,22 @@ namespace CarGame
         public WheelCollider onsoltekerlek, onsagtekerlek, arkasoltekerlek, arkasagtekerlek;
         public float motorhizi, donmehizi;
         public Joystick tus;
-        public GameObject Gold;
+        public GameObject gold;
         public Transform konum1, konum2, konum3;
-        ArabaSatinAl market;
+        public float spawnDuration = 4.0f;
+        public int diamondMoney = 100;
 
+
+
+        private ArabaSatinAl _market;
+
+
+        public const string diamond_tag = "Gold";
 
         private void Start()
         {
-            InvokeRepeating("clon", 0, 4.0f);
+            InvokeRepeating("clon", 0, spawnDuration);
+            _market = GameObject.FindGameObjectWithTag("Para").GetComponent<ArabaSatinAl>();
         }
 
         void Update()
@@ -34,30 +42,110 @@ namespace CarGame
 
         public void clon()
         {
-            if(tus.Vertical > 0)
+            if (tus.Vertical > 0)
             {
-                GameObject goldobje = Instantiate(Gold, konum1.transform.position, Quaternion.identity);
-                GameObject goldobje2 = Instantiate(Gold, konum2.transform.position, Quaternion.identity);
-                GameObject goldobje3 = Instantiate(Gold, konum3.transform.position, Quaternion.identity);
+                GameObject goldobje = Instantiate(gold, konum1.transform.position, Quaternion.identity);
+                GameObject goldobje2 = Instantiate(gold, konum2.transform.position, Quaternion.identity);
+                GameObject goldobje3 = Instantiate(gold, konum3.transform.position, Quaternion.identity);
                 Debug.Log("altýn objesi oluþturdu");
 
 
             }
-            
+
         }
 
         public void OnTriggerEnter(Collider nesne)
         {
-            if(nesne.gameObject.tag == "Gold")
+            if (nesne.gameObject.tag == diamond_tag)
             {
-                market = GameObject.FindGameObjectWithTag("Para").GetComponent<ArabaSatinAl>();
-                market.para += 100;
+
+                _market.para += diamondMoney;
+                Debug.Log("Altýn toplandý - " + nesne.GetInstanceID() + " - " + _market.para);
                 Destroy(nesne.transform.gameObject);
             }
-            
+
         }
 
 
+    }
+
+    public class GameManager : MonoBehaviour
+    {
+        public CarController carController;
+        public GoldSpawner goldSpawner;
+        public int goldCount;
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                goldSpawner.SpawnGold(goldCount);
+                carController.StartCarMovement();
+            }
+        }
+    }
+
+    public class CarController : MonoBehaviour
+    {
+        public CarMovement carMovement;
+
+        public void StartCarMovement()
+        {
+            carMovement.canMove = true;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Finish"))
+            {
+                carMovement.canMove = false;
+            }
+        }
+    }
+    public class CarMovement : MonoBehaviour
+    {
+        public WheelCollider onsoltekerlek, onsagtekerlek, arkasoltekerlek, arkasagtekerlek;
+        public float motorhizi, donmehizi;
+        public Joystick tus;
+
+        public bool canMove;
+
+        void Update()
+        {
+            if (canMove)
+            {
+
+                //ileri geri gitme
+                arkasagtekerlek.motorTorque = motorhizi * -tus.Vertical;
+                arkasoltekerlek.motorTorque = motorhizi * -tus.Vertical;
+                //saða sola dönme
+                onsagtekerlek.steerAngle = donmehizi * tus.Horizontal;
+                onsoltekerlek.steerAngle = donmehizi * tus.Horizontal;
+            }
+
+
+
+        }
+    }
+
+    public class GoldSpawner : MonoBehaviour
+    {
+        public Gold goldRes;
+
+        public void SpawnGold(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+
+                Instantiate(goldRes, transform.position, Quaternion.identity);
+            }
+        }
+
+    }
+
+    public class Gold : MonoBehaviour
+    {
+        public int score;
     }
 }
 
