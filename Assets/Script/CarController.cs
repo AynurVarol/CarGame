bfa171a85d8d9703437094d4162965cd765b469c
Aynur-AcerLaptop <aynurvarol369@gmail.com>
2023-05-12 13:29:32 +0300

    Car Controller Script

---
+++ Assets/Script/CarController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static CarGame.ArabaHareket;
+
+
+namespace CarGame
+{
+    public class CarController : MonoBehaviour
+    {
+        // Start is called before the first frame update
+
+        public CarMovement carMovement;
+
+        public void StartCarMovement()
+        {
+            carMovement.canMove = true;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Finish"))
+            {
+                carMovement.canMove = false;
+            }
+        }
+
+
+
+
+
+
+    }
+} 
