e65469c5f5541bd2afcd3e03f1456b22218870fb
Aynur-AcerLaptop <aynurvarol369@gmail.com>
2023-05-12 13:28:28 +0300

    edit: ArabaHareketScript

--- Assets/Script/ArabaHareket.cs
+++ Assets/Script/ArabaHareket.cs
@@ -9,17 +9,17 @@ namespace CarGame
         public WheelCollider onsoltekerlek, onsagtekerlek, arkasoltekerlek, arkasagtekerlek;
         public float motorhizi, donmehizi;
         public Joystick tus;
-        public GameObject gold;
-        public Transform konum1, konum2, konum3;
         public float spawnDuration = 4.0f;
-        public int diamondMoney = 100;
+        public bool canMove;
+        public GoldClon goldClon;
+        public GameObject gold;
 
 
 
         private ArabaSatinAl _market;
 
 
-        public const string diamond_tag = "Gold";
+
 
         private void Start()
         {
@@ -29,127 +29,52 @@ namespace CarGame
 
         void Update()
         {
-
-            //ileri geri gitme
-            arkasagtekerlek.motorTorque = motorhizi * -tus.Vertical;
-            arkasoltekerlek.motorTorque = motorhizi * -tus.Vertical;
-            //saða sola dönme
-            onsagtekerlek.steerAngle = donmehizi * tus.Horizontal;
-            onsoltekerlek.steerAngle = donmehizi * tus.Horizontal;
-
-
-        }
-
-        public void clon()
-        {
-            if (tus.Vertical > 0)
+            if(canMove)
             {
-                GameObject goldobje = Instantiate(gold, konum1.transform.position, Quaternion.identity);
-                GameObject goldobje2 = Instantiate(gold, konum2.transform.position, Quaternion.identity);
-                GameObject goldobje3 = Instantiate(gold, konum3.transform.position, Quaternion.identity);
-                Debug.Log("altýn objesi oluþturdu");
-
-
-            }
-
-        }
-
-        public void OnTriggerEnter(Collider nesne)
-        {
-            if (nesne.gameObject.tag == diamond_tag)
-            {
-
-                _market.para += diamondMoney;
-                Debug.Log("Altýn toplandý - " + nesne.GetInstanceID() + " - " + _market.para);
-                Destroy(nesne.transform.gameObject);
-            }
-
-        }
-
-
-    }
-
-    public class GameManager : MonoBehaviour
-    {
-        public CarController carController;
-        public GoldSpawner goldSpawner;
-        public int goldCount;
-
-        private void Update()
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                goldSpawner.SpawnGold(goldCount);
-                carController.StartCarMovement();
-            }
-        }
-    }
-
-    public class CarController : MonoBehaviour
-    {
-        public CarMovement carMovement;
-
-        public void StartCarMovement()
-        {
-            carMovement.canMove = true;
-        }
-
-        private void OnTriggerEnter(Collider other)
-        {
-            if (other.CompareTag("Finish"))
-            {
-                carMovement.canMove = false;
-            }
-        }
-    }
-    public class CarMovement : MonoBehaviour
-    {
-        public WheelCollider onsoltekerlek, onsagtekerlek, arkasoltekerlek, arkasagtekerlek;
-        public float motorhizi, donmehizi;
-        public Joystick tus;
-
-        public bool canMove;
-
-        void Update()
-        {
-            if (canMove)
-            {
-
-                //ileri geri gitme
                 arkasagtekerlek.motorTorque = motorhizi * -tus.Vertical;
                 arkasoltekerlek.motorTorque = motorhizi * -tus.Vertical;
                 //saða sola dönme
                 onsagtekerlek.steerAngle = donmehizi * tus.Horizontal;
                 onsoltekerlek.steerAngle = donmehizi * tus.Horizontal;
+
             }
 
-
+            
 
         }
-    }
+         public void Clon()
+         {
+            goldClon = gold.GetComponent<GoldClon>();
 
-    public class GoldSpawner : MonoBehaviour
-    {
-        public Gold goldRes;
-        public Vector3 offset =new Vector3(2,0,2);
+         }
 
-        public void SpawnGold(int count)
+        
+
+      
+
+        public class GoldSpawner : MonoBehaviour
         {
-            for (int i = 0; i < count; i++)
+            public Gold goldRes;
+            public Vector3 offset = new Vector3(2, 0, 2);
+
+            public void SpawnGold(int count)
             {
-                var pos = transform.position + (offset * i);
-                // 1. pos = (0,0,0)
-                //2. pos =  (2,0,2)
-                //3.pos = (4,0,4)
-                Instantiate(goldRes, pos, Quaternion.identity);
+                for (int i = 0; i < count; i++)
+                {
+                    var pos = transform.position + (offset * i);
+                    // 1. pos = (0,0,0)
+                    //2. pos =  (2,0,2)
+                    //3.pos = (4,0,4)
+                    Instantiate(goldRes, pos, Quaternion.identity);
+                }
             }
+
         }
 
-    }
-
-    public class Gold : MonoBehaviour
-    {
-        public int score;
+        public class Gold : MonoBehaviour
+        {
+            public int score;
+        }
     }
 }
 
